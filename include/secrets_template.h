5e0b28578c4b2fedff9fc048998a07766726e344
Maxwell Fulton <alpinechili@gmail.com>
2026-07-31 15:41:11 +0100

    Add gitignore and secrets template

---
+++ include/secrets_template.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// WiFi
+constexpr char SECRET_WIFI_SSID[] = "YOUR_WIFI_NAME";
+constexpr char SECRET_WIFI_PASSWORD[] = "YOUR_WIFI_PASSWORD";
+
+// MQTT
+constexpr char SECRET_MQTT_SERVER[] = "192.168.0.10";
+constexpr char SECRET_MQTT_PORT = 1883;
+constexpr char SECRET_MQTT_USER[] = "mqtt";
+constexpr char SECRET_MQTT_PASSWORD[] = "password";
+
+// Device
+constexpr char DEVICE_NAME[] = "VictronCT";
+
+// Victron
+constexpr char VICTRON_MODBUS_PORT = 502;
