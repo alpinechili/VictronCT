88113804449de580014205115c017da3d7f4e830
Maxwell Fulton <alpinechili@gmail.com>
2026-08-05 17:50:45 +0100

    Add Modbus write API

--- include/modbus_manager.h
+++ include/modbus_manager.h
@@ -9,25 +9,42 @@ void modbusLoop();
 
 bool modbusConnected();
 
-bool readHoldingRegister(uint8_t unitId,
-                         uint16_t address,
-                         uint16_t& value);
+//--------------------------------------------------
+// Read Registers
+//--------------------------------------------------
 
-bool readHoldingRegisters(uint8_t unitId,
-                          uint16_t startAddress,
-                          uint16_t count,
-                          uint16_t* values);
+bool readHoldingRegister(
+    uint8_t unitId,
+    uint16_t address,
+    uint16_t& value);
 
-bool readRegister(uint8_t unitId,
-                  const Victron::RegisterInfo& reg,
-                  float& value);
+bool readHoldingRegisters(
+    uint8_t unitId,
+    uint16_t startAddress,
+    uint16_t count,
+    uint16_t* values);
+
+bool readRegister(
+    uint8_t unitId,
+    const Victron::RegisterInfo& reg,
+    float& value);
+
+//--------------------------------------------------
+// Write Registers
+//--------------------------------------------------
+
+bool writeHoldingRegister(
+    uint8_t unitId,
+    uint16_t address,
+    uint16_t value);
 
 //--------------------------------------------------
 // Register Cache
 //--------------------------------------------------
 
-bool getCachedRegister(uint8_t unitId,
-                       uint16_t address,
-                       uint16_t& value);
+bool getCachedRegister(
+    uint8_t unitId,
+    uint16_t address,
+    uint16_t& value);
 
 void clearRegisterCache();
